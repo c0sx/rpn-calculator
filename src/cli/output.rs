8920dc8626ead3d46c457a673b3c888669894b6b
tweek <vallkhale@gmail.com>
2021-02-19 21:16:36 +0300

    fix: drop unused code

--- src/cli/output.rs
+++ src/cli/output.rs
@@ -2,10 +2,6 @@ pub fn output_string(s: &str) {
     println!("{}", s);
 }
 
-pub fn output_double(d: f64) {
-    println!("{}", d);
-}
-
 pub fn output_results(tokens: Vec<char>, result: f64) {
     let notation = tokens
         .iter()
